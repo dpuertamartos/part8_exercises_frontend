92e65007eb56cb93dfb13beb565f7b85422c3b73
David Puerta Martos <dpuerta@usal.es>
2022-11-25 18:44:50 +0100

    exercise 8 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { useQuery } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  
 
   return (
     <div>
